87e359fabd52cbdfd7e7b38d8065225793ec88e1
sadiwantoro <shadiwantoro@gmail.com>
2023-06-27 20:46:36 -0700

    Firstupload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Brief description of your web page.">
+  <title>Page Title</title>
+</head>
+<body>
+  <!-- Your web page content here -->
+</body>
+</html>
